0baec0b6122fc068d722a800f2bfbf0da8f7c511
Kevin Vuong <kevinvuong99@hotmail.com>
2019-10-26 19:46:42 -0700

    Add files via upload

---
+++ student_starter/24Game_ss.py
@@ -0,0 +1,21 @@
+# 24 game
+
+# import statements can live here
+
+
+def main():
+    print("Starting new game of 24")
+    # TODO: get seed
+    # TODO: generate numbers
+    # TODO: print numbers
+    print("\n")
+    # TODO: set loop to play again or not (doesn't have to be here)
+    # TODO: get expression
+    # prompt: Please enter an expression with a space after every number and operator.
+    # TODO: set loop OR conditions for where to keep prompting for new expression (doesn't have to be here)
+    # prompt: Invalid Expression. Please enter a valid one.
+    # TODO: compute and print results depending on whether it equals 24 or not
+    # TODO: ask about playing again
+
+
+main()
